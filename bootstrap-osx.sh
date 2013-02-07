8e8845f426e30378df68e9a007e362fde7af6efa
Timur Batyrshin <timur.batyrshin@flatstack.com>
2013-02-07 17:22:18 +0400

    shell style fix

--- bootstrap-osx.sh
+++ bootstrap-osx.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-if [ -d /Applications/Sublime\ Text\ 2.app/ ]
+APP_DIR="/Applications/Sublime Text 2.app"
+SUBLIME_DIR="$HOME/Library/Application Support/Sublime Text 2"
+
+if [ -d "$APP_DIR" ]
 then
   echo 'Sublime Text 2 application found.'
 else
@@ -10,48 +13,46 @@ else
 fi
 
 # Add `subl` terminal command
-if [ `which subl` ]
+if [ $(which subl > /dev/null) ]
 then
   echo '`subl` terminal command already installed'
 else
-  if [ -d $HOME/.bin ]
+  if [ -d "$HOME/.bin" ]
   then
-    ln -s /Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin/subl $HOME/.bin/subl
+    ln -s "$APP_DIR/Contents/SharedSupport/bin/subl" "$HOME/.bin/subl"
   else
-    ln -s /Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin/subl /usr/local/bin/subl
+    ln -s "$APP_DIR/Contents/SharedSupport/bin/subl" /usr/local/bin/subl
   fi
 fi
 
 # Install package control
 # http://wbond.net/sublime_packages/package_control/installation
 
-if [ -d $HOME/Library/Application\ Support/Sublime\ Text\ 2/ ]
+if [ -d "$SUBLIME_DIR" ]
 then
   echo 'Config directory found.'
 else
   echo 'Creating config directory'
-  subl -b && sleep '0.5' && osascript -e 'tell application "Sublime Text 2" to quit'
+  subl --background && sleep '0.5' && osascript -e 'tell application "Sublime Text 2" to quit'
 fi
 
-cd $HOME/Library/Application\ Support/Sublime\ Text\ 2/
-
-if [ -f Installed\ Packages/Package\ Control.sublime-package ]
+if [ -f "$SUBLIME_DIR/Installed Packages/Package Control.sublime-package" ]
 then
   echo 'Package Control already installed.'
 else
   echo 'Downloading Package Control'
-  curl -s 'http://sublime.wbond.net/Package%20Control.sublime-package' > Installed\ Packages/Package\ Control.sublime-package
+  curl --silent --show-error 'http://sublime.wbond.net/Package%20Control.sublime-package' --output "$SUBLIME_DIR/Installed Packages/Package Control.sublime-package"
 fi
-
-cd -
 
 echo 'Add default packages and configs...'
 
-cp -f settings/Preferences.sublime-settings      $HOME/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/
-cp -f settings/Package\ Control.sublime-settings $HOME/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/
-cp -f settings/Default\ \(OSX\).sublime-keymap   $HOME/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/
+# consider replacing by `cp settings/* $SUBLIME_DIR/Packages/User/'
+cp -f "settings/Preferences.sublime-settings" \
+      "settings/Package Control.sublime-settings" \
+      "settings/Default (OSX).sublime-keymap" \
+      "$SUBLIME_DIR/Packages/User/"
 
-echo | subl -w <<TXT
+echo | subl --wait <<TXT
 # Almost done
 
 Now Sublime Text 2 installing packages and "Soda Theme"
